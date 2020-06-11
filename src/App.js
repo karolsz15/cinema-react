76250aef8021c9a0f836262c31659207ea9b6584
Karol <karolsz15@gmail.com>
2020-06-11 21:54:14 +0200

    react app setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
+
+// import Courses from './containers/Courses/Courses';
+// import Users from './containers/Users/Users';
+
+class App extends Component {
+  render () {
+    return (
+    //   <BrowserRouter>
+        <div>test test</div>
+    //   </BrowserRouter>
+    );
+  }
+}
+
+export default App;
