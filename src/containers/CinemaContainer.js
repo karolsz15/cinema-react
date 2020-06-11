d24d72cb2dd7b1a8ee7ee303781df59027f2eea9
Karol <karolsz15@gmail.com>
2020-06-12 00:23:57 +0200

    fixed 404 defauld display

--- src/containers/CinemaContainer.js
+++ src/containers/CinemaContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Homepage from '../components/Homepage/Homepage';
 import Menu from '../components/Menu/Menu';
@@ -15,11 +15,13 @@ class CinemaContainer extends Component {
         <React.Fragment>
             <div class="full">
                 <Menu />
-                <Route path ="/" exact component={Homepage} />
-                <Route path ="/trailers" component={Trailers} />
-                <Route path ="/booking" component={Booking} />
-                <Route path ="/contact" component={Contact} />
-                <Route render={() => <h1>404: Page not found</h1>} />
+                <Switch>
+                    <Route path ="/" exact component={Homepage} />
+                    <Route path ="/trailers" component={Trailers} />
+                    <Route path ="/booking" component={Booking} />
+                    <Route path ="/contact" component={Contact} />
+                    <Route render={() => <h1>404: Page not found</h1>} />
+                </Switch>
             </div>
         </React.Fragment>
         );
