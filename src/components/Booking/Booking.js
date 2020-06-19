a3a1df7a5b183aa428ca69d2f8521ed19673bc78
Karol <karolsz15@gmail.com>
2020-06-19 11:26:41 +0200

    fixed weekday display bug in Booking and BookingModal

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -8,15 +8,17 @@ import BookingSelection from './BookingSelection/BookingSelection';
 
 class Booking extends Component {
 
-	// let fullDate = new Date();
-
 	state = {
         modalVisible: false,
         error: false,
 		movies: null,
 		reservations: null,
-		activeDay: new Date().getDay(),
-		hours: ['08:00', '10:00', '12:00', '14:00', '18:00', '20:00', '22:00'],
+		activeDay: ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"][new Date().getDay()],
+		hours: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
+				'06:00', '07:00', '08:00', '09:00', '10:00',
+				'11:00', '12:00', '13:00', '14:00', '15:00',
+				'16:00', '17:00','18:00', '19:00', '20:00',
+				'21:00', '22:00', '23:00', '24:00'],
 		activeHour: null,
 		activeTitle: null
     }
