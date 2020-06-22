e0ae35c91e618d3e1e9dd02045bae8834712898e
Karol <karolsz15@gmail.com>
2020-06-22 19:06:01 +0200

    fixed slideshow and posters display on desktop

--- src/components/Booking/OneMovie.js
+++ src/components/Booking/OneMovie.js
@@ -4,13 +4,11 @@ const oneMovie = props => {
     return (
         <React.Fragment>
             <div className="booking_object">
-                <img src={props.poster} className="mr-3" alt="..."></img>
+                <img src={props.poster} alt="..." style={{margin: '.5em'}}></img>
                 <div className="media-body">
-                    <h5 className="mt-0">{props.title}</h5>
-                    <br />
+                    <h5 style={{margin: '.5em'}}>{props.title}</h5>
                     {props.summary}
-                    <br />
-                    <div className="mt-5" >
+                    <div>
                         <a style={{margin: '.5em'}} onClick={props.clicked1} className="btn btn-secondary" href="#selection">{props.hour1}</a>
                         <a style={{margin: '.5em'}} onClick={props.clicked2} className="btn btn-secondary" href="#selection">{props.hour2}</a>
                     </div>
