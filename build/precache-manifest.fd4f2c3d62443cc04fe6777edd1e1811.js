e1a431fd356ea5141f0ad1887e4c518cf660be66
Karol <karolsz15@gmail.com>
2020-06-23 20:03:06 +0200

    added firebase hosting files

---
+++ build/precache-manifest.fd4f2c3d62443cc04fe6777edd1e1811.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84e9ede84a58f6fccf8a51971884074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4516eeae2e08ac8112c",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "685c3520724bb7ebf801",
+    "url": "/static/css/main.dd9799ce.chunk.css"
+  },
+  {
+    "revision": "c4516eeae2e08ac8112c",
+    "url": "/static/js/2.7add943f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7add943f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685c3520724bb7ebf801",
+    "url": "/static/js/main.30e70033.chunk.js"
+  },
+  {
+    "revision": "499b05b621fe23973604",
+    "url": "/static/js/runtime-main.c2cb97e7.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
