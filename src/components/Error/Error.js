df6781e8c848a96272d6228f3bfe54acb952c947
Karol <karolsz15@gmail.com>
2020-07-08 22:57:29 +0200

    improved 404 error handling

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -14,16 +14,8 @@ const error = props => (
 
         <div class="contact-content">
             <div class="main-contact">
-                <h3 class="head">ERROR 404</h3>
-                <p>page not found :(</p>
-
-                {/* <div class="contact_info">
-                    {/* <h3>Find Us Here</h3> */}
-                    {/* <div class="map"> */}
-                        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2561.321380879272!2d19.935162715717375!3d50.061542629423606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b0e0789882b%3A0x3d71c4eb22dfdb51!2zUnluZWsgR8WCw7N3bnksIEtyYWvDs3c!5e0!3m2!1spl!2spl!4v1578239272803!5m2!1spl!2spl" width="600" height="450" frameborder="0" allowfullscreen=""></iframe> */}
-                    {/* </div> */}
-            {/* </div> */} */}
-        </div>
+                <img class="errorImg" src="https://firebasestorage.googleapis.com/v0/b/karol-cinema.appspot.com/o/error404.jpg?alt=media&token=cbb825e6-bc0c-408f-a317-1552486bd90e"></img>
+            </div>
     </div>
 </div>
 
