f9f942e2c9869cf64c69d5f796d74324e93b7db1
Karol <karolsz15@gmail.com>
2020-07-08 15:18:45 +0200

    implemented Redux in Trailers

--- src/components/Trailers/Trailers.js
+++ src/components/Trailers/Trailers.js
@@ -4,43 +4,37 @@ import Spinner from '../UI/Spinner/Spinner';
 
 import axios from 'axios';
 import _ from 'lodash';
+import {connect} from 'react-redux';
 
 class Trailers extends Component {
 
-    state = {
-        showModal: false,
-        error: false,
-        movies: null
-    }
+    // NOW HANDLED WITH REDUX
+    // state = {
+    //     showModal: false,
+    //     error: false,
+    //     movies: null
+    // }
 
     componentDidMount() {
         axios
           .get("https://karol-cinema.firebaseio.com/test.json")
           .then(response => {
-            this.setState({
-                movies: response.data
-            });
+            this.props.setMovies(response.data);
           })
-          .catch(error => this.setState({ error: true }));
-    }
-
-    renderModal = () => {
-        this.setState({showModal: true});
+          .catch(error => this.props.setError);
     }
 
     render() {
 
-        let movies = this.state.error ? <p>Ingredients cant be loaded</p> : <Spinner />;
+        let movies = this.props.trailersError ? <p>Ingredients cant be loaded</p> : <Spinner />;
 
-        /*
-        limitMovieDescriptionTitle
+        // limitMovieDescriptionTitle()
         // 'Pasta with tomato and spinach'
-        acc: 0 / acc + cur.length = 5 / newDesc = ['Pasta']
-        acc: 5 / acc + cur.length = 9 / newDesc = ['Pasta', 'with']
-        acc: 9 / acc + cur.length = 15 / newDesc = ['Pasta', 'with', 'tomato']
-        acc: 15 / acc + cur.length = 18 / newDesc = ['Pasta', 'with', 'tomato']
-        acc: 18 / acc + cur.length = 24 / newDesc = ['Pasta', 'with', 'tomato']
-        */
+        // acc: 0 / acc + cur.length = 5 / newDesc = ['Pasta']
+        // acc: 5 / acc + cur.length = 9 / newDesc = ['Pasta', 'with']
+        // acc: 9 / acc + cur.length = 15 / newDesc = ['Pasta', 'with', 'tomato']
+        // acc: 15 / acc + cur.length = 18 / newDesc = ['Pasta', 'with', 'tomato']
+        // acc: 18 / acc + cur.length = 24 / newDesc = ['Pasta', 'with', 'tomato']
 
         const limitMovieDescriptionTitle = (desc, limit = 350) => {
             const newDesc = [];
@@ -57,9 +51,9 @@ class Trailers extends Component {
             return desc;
         }
 
-        if (this.state.movies) {
+        if (this.props.trailersMovies) {
 
-            const moviesArray = _.values(this.state.movies);
+            const moviesArray = _.values(this.props.trailersMovies);
 
             movies = (
                 <div className="content-grids">
@@ -102,4 +96,21 @@ class Trailers extends Component {
     }
 };
 
-export default Trailers;
+const mapStateToProps = state => {
+    return {
+        trailersModalVisible: state.modalVisible,
+        trailersError: state.error,
+        trailersMovies: state.movies
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        showModal: () => dispatch({type: 'SHOW_MODAL'}),
+        hideModal: () => dispatch({type: 'HIDE_MODAL'}),
+        setMovies: (data) => dispatch({type: 'SET_MOVIES', data: data}),
+        setError: () => dispatch({type: 'ERROR'})
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Trailers);
