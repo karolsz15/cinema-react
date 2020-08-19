c0c83f546df0659b190be4ae7e7e8e29a33bb700
Karol Szpek <karolsz15@gmail.com>
2020-08-19 18:11:54 +0200

    changed AutoPlay into functional component

--- src/components/AutoPlay/AutoPlay.js
+++ src/components/AutoPlay/AutoPlay.js
@@ -1,36 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import PropTypes from 'prop-types';
 
-class AutoPlay extends Component {
+const AutoPlay = props => {
 
-  render() {
-
-    const settings = {
-      dots: false,
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 2000,
-      autoplaySpeed: 2000,
-      cssEase: "linear"
-    };
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
     
-    const posters = this.props.postersArray.map(el => (
-      <div key={el}>
-        <img src={el} alt="movie poster" className="sliderImage" />
-      </div>
-    ));
+  const posters = props.postersArray.map(el => (
+    <div key={el}>
+      <img src={el} alt="movie poster" className="sliderImage" />
+    </div>
+  ));
 
-    return (
-      <div className="sliderContainer" style={{padding: '1em'}}>
-        <Slider {...settings}>
-          {posters}
-        </Slider>
-      </div>
-    );
-  }
+  return (
+    <div className="sliderContainer" style={{padding: '1em'}}>
+      <Slider {...settings}>
+        {posters}
+      </Slider>
+    </div>
+  );
 };
 
 AutoPlay.propTypes = {
