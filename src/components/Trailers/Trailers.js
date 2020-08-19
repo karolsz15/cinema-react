6ddd8fc0bd5e473abdd44a4e91de5e04d4f539d0
Karol Szpek <karolsz15@gmail.com>
2020-08-19 18:08:39 +0200

    changed to functional component, added useEffect and Redux hooks

--- src/components/Trailers/Trailers.js
+++ src/components/Trailers/Trailers.js
@@ -1,116 +1,97 @@
-import React, {Component} from 'react';
+import React, { useEffect, useCallback } from 'react';
 import SingleMovie from './SingleMovie/SingleMovie';
 import Spinner from '../UI/Spinner/Spinner';
 
 import axios from 'axios';
 import _ from 'lodash';
-import {connect} from 'react-redux';
+import {useSelector, useDispatch } from 'react-redux';
 
-class Trailers extends Component {
+const Trailers = () => {
 
-    // NOW HANDLED WITH REDUX
-    // state = {
-    //     showModal: false,
-    //     error: false,
-    //     movies: null
-    // }
+    //map state to consts
+    const trailersError = useSelector(state => state.error);
+    const trailersMovies = useSelector(state => state.movies);
 
-    componentDidMount() {
+    //map dispatch to consts
+    const dispatch = useDispatch();
+    const setMovies = useCallback( (data) => dispatch({type: 'SET_MOVIES', data: data}) , [dispatch] );
+    const setError = useCallback( () => dispatch({type: 'ERROR'}) , [dispatch] );
+
+    useEffect(() => {
         axios
-          .get("https://karol-cinema.firebaseio.com/test.json")
-          .then(response => {
-            this.props.setMovies(response.data);
-          })
-          .catch(error => this.props.setError);
-    }
+        .get("https://karol-cinema.firebaseio.com/test.json")
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error => setError);
+    }, [setMovies, setError]);
 
-    render() {
+    let movies = trailersError ? <p>Ingredients cant be loaded</p> : <Spinner />;
 
-        let movies = this.props.trailersError ? <p>Ingredients cant be loaded</p> : <Spinner />;
+    // limitMovieDescriptionTitle()
+    // e.g. 'Pasta with tomato and spinach' -> 'Pasta with tomato'
+    // acc: 0 / acc + cur.length = 5 / newDescriptionArray = ['Pasta']
+    // acc: 5 / acc + cur.length = 9 / newDescriptionArray = ['Pasta', 'with']
+    // acc: 9 / acc + cur.length = 15 / newDescriptionArray = ['Pasta', 'with', 'tomato']
+    // acc: 15 / acc + cur.length = 18 / newDescriptionArray = ['Pasta', 'with', 'tomato']
+    // acc: 18 / acc + cur.length = 24 / newDescriptionArray = ['Pasta', 'with', 'tomato']
 
-        // limitMovieDescriptionTitle()
-        // 'Pasta with tomato and spinach'
-        // acc: 0 / acc + cur.length = 5 / newDesc = ['Pasta']
-        // acc: 5 / acc + cur.length = 9 / newDesc = ['Pasta', 'with']
-        // acc: 9 / acc + cur.length = 15 / newDesc = ['Pasta', 'with', 'tomato']
-        // acc: 15 / acc + cur.length = 18 / newDesc = ['Pasta', 'with', 'tomato']
-        // acc: 18 / acc + cur.length = 24 / newDesc = ['Pasta', 'with', 'tomato']
-
-        const limitMovieDescriptionTitle = (desc, limit = 350) => {
-            const newDesc = [];
-            if (desc.length > limit) {
-                desc.split(' ').reduce((acc, cur) => {
-                    if (acc + cur.length <= limit) {
-                        newDesc.push(cur);
-                    }
-                    return acc + cur.length;
-                }, 0);
-
-                return `${newDesc.join(' ')} ...`;
-            }
-            return desc;
-        }
-
-        if (this.props.trailersMovies) {
-
-            const moviesArray = _.values(this.props.trailersMovies);
-
-            movies = (
-                <div className="content-grids">
-                    {moviesArray.map(el => (
-                        <SingleMovie 
-                                title={el.title} 
-                                summary={limitMovieDescriptionTitle(el.summary)} 
-                                poster={el.posterUrl} 
-                                trailer={el.trailerUrl}
-                                key={el.title}
-                        />
-                    ))}
-                </div>
-            );
+    const limitMovieDescriptionTitle = (description, limit = 350) => {
+        const newDescriptionArray = [];
+        if (description.length > limit) {
+            description.split(' ').reduce((acc, cur) => {
+                if (acc + cur.length <= limit) {
+                    newDescriptionArray.push(cur);
+                }
+                return acc + cur.length;
+            }, 0);
+            return `${newDescriptionArray.join(' ')} ...`;
         };
+        return description;
+    };
 
-        return (
-            <React.Fragment>
-                <div className="main">
-                    <div className="video-content">
-                        <div className="top-header span_top">
-                            <div className="logo">
-                                <a href="/"><img src="images/logo4.png" alt="" /></a>
-                                <p>Ticket booking app</p>
-                            </div>
-                            <div className="clearfix"></div>
-                        </div>
-                        <div className="right-content">
-                            <div className="right-content-heading">
-                                <div className="right-content-heading-left">
-                                    <h3 className="head">Trailers of latest movies</h3>
-                                </div>
-                            </div>
-                        </div>
-                            {movies}
-                        </div>
-                    </div>   
-            </React.Fragment>
+    if (trailersMovies) {
+
+        const moviesArray = _.values(trailersMovies);
+
+        movies = (
+            <div className="content-grids">
+                {moviesArray.map(el => (
+                    <SingleMovie 
+                        title={el.title} 
+                        summary={limitMovieDescriptionTitle(el.summary)} 
+                        poster={el.posterUrl} 
+                        trailer={el.trailerUrl}
+                        key={el.title}
+                    />
+                ))}
+            </div>
         );
-    }
-};
-
-const mapStateToProps = state => {
-    return {
-        trailersModalVisible: state.modalVisible,
-        trailersError: state.error,
-        trailersMovies: state.movies
     };
+
+    return (
+        <React.Fragment>
+            <div className="main">
+                <div className="video-content">
+                    <div className="top-header span_top">
+                        <div className="logo">
+                            <a href="/"><img src="images/logo4.png" alt="" /></a>
+                            <p>Ticket booking app</p>
+                        </div>
+                        <div className="clearfix"></div>
+                    </div>
+                    <div className="right-content">
+                        <div className="right-content-heading">
+                            <div className="right-content-heading-left">
+                                <h3 className="head">Trailers of latest movies</h3>
+                            </div>
+                        </div>
+                    </div>
+                        {movies}
+                    </div>
+                </div>   
+        </React.Fragment>
+    );
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        showModal: () => dispatch({type: 'SHOW_MODAL'}),
-        hideModal: () => dispatch({type: 'HIDE_MODAL'}),
-        setMovies: (data) => dispatch({type: 'SET_MOVIES', data: data}),
-        setError: () => dispatch({type: 'ERROR'})
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Trailers);
+export default Trailers;
